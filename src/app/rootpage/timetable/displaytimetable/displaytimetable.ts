a65ffc08c25a936b0c1d69d96dcaece32c65fdfc
ShanmukhVegi <62086771+ShanmukhVegi@users.noreply.github.com>
2021-04-17 09:28:09 +0530

    added display timetable backend

--- src/app/rootpage/timetable/displaytimetable/displaytimetable.ts
+++ src/app/rootpage/timetable/displaytimetable/displaytimetable.ts
@@ -3,6 +3,7 @@ import {Router} from "@angular/router";
 import { CommonModule } from '@angular/common';
 import {NgModule} from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
+import {ActivatedRoute} from "@angular/router";
 
 const firebase = require("nativescript-plugin-firebase/app");
 
@@ -15,7 +16,7 @@ const firebase = require("nativescript-plugin-firebase/app");
     imports:[CommonModule,BrowserModule]
 })
 export class displaytimetableComponent {
-    obj;
+    monday; tuesday; wednesday; thursday; friday; saturday; sunday;
     rows;
     moncol;
     tuecol;
@@ -23,48 +24,107 @@ export class displaytimetableComponent {
     thucol;
     fricol;
     satcol;
-    public constructor(private router:Router){
+    class;
+    vals : Array<JSON> =[];
+    public constructor(private router:Router,private route: ActivatedRoute){
+        this.route.params.subscribe((params)=>{
+            this.class=params["name"];
+        });
         var Wday: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
         var day = new Date();
         var TodayDay = Wday[day.getDay()];
-        this.obj={
-            "Mon":
-            [{"Time": "1-2", "period":"Maths"},
-            {"Time": "2-3", "period":"Physics"},
-            {"Time": "3-4", "period":"Chemistry"},
-            {"Time": "4-5", "period":"Biology"},
-            {"Time": "5-6", "period":"English"}],
-            "Tue":
-            [{"Time": "8-9", "period":"Maths"},
-            {"Time": "9-10", "period":"Physics"},
-            {"Time": "10-11", "period":"Chemistry"},
-            {"Time": "11-12", "period":"Biology"},
-            {"Time": "12-13", "period":"English"}],
-            "Wed":
-            [{"Time": "1-2", "period":"Maths"},
-            {"Time": "2-3", "period":"Physics"},
-            {"Time": "3-4", "period":"Chemistry"},
-            {"Time": "4-5", "period":"Biology"},
-            {"Time": "5-6", "period":"English"}],
-            "Thu":
-            [{"Time": "8-9", "period":"Maths"},
-            {"Time": "9-10", "period":"Physics"},
-            {"Time": "10-11", "period":"Chemistry"},
-            {"Time": "11-12", "period":"Biology"},
-            {"Time": "12-13", "period":"English"}],
-            "Fri":
-            [{"Time": "1-2", "period":"Maths"},
-            {"Time": "2-3", "period":"Physics"},
-            {"Time": "3-4", "period":"Chemistry"},
-            {"Time": "4-5", "period":"Biology"},
-            {"Time": "5-6", "period":"English"}],
-            "Sat":
-            [{"Time": "8-9", "period":"Maths"},
-            {"Time": "9-10", "period":"Physics"},
-            {"Time": "10-11", "period":"Chemistry"},
-            {"Time": "11-12", "period":"Biology"},
-            {"Time": "12-13", "period":"English"}]
-        };
+        var getdata = firebase.firestore().collectionGroup("Monday");
+        var check = getdata.where("Class_Id","==","1000");
+        check.get().then(result=>{
+            result.forEach(doc=>{
+                console.log(JSON.stringify(doc.data()));
+                this.vals.push(doc.data());
+            })
+        })
+        console.log(this.vals);
+        this.rows={};
+        this.vals.sort(function(a,b){
+            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
+        })
+        this.monday = this.vals;
+
+        this.vals.length=0;
+        getdata = firebase.firestore().collectionGroup("Tuesday");
+        check = getdata.where("Class_Id","==","1000");
+        check.get().then(result=>{
+            result.forEach(doc=>{
+                console.log(JSON.stringify(doc.data()));
+                this.vals.push(doc.data());
+            })
+        })
+        console.log(this.vals);
+        this.vals.sort(function(a,b){
+            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
+        })
+        this.tuesday = this.vals;
+
+        this.vals.length=0;
+        getdata = firebase.firestore().collectionGroup("Wednesday");
+        check = getdata.where("Class_Id","==","1000");
+        check.get().then(result=>{
+            result.forEach(doc=>{
+                console.log(JSON.stringify(doc.data()));
+                this.vals.push(doc.data());
+            })
+        })
+        console.log(this.vals);
+        this.vals.sort(function(a,b){
+            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
+        })
+        this.wednesday = this.vals;   
+        
+        
+        this.vals.length=0;
+        getdata = firebase.firestore().collectionGroup("Thursday");
+        check = getdata.where("Class_Id","==","1000");
+        check.get().then(result=>{
+            result.forEach(doc=>{
+                console.log(JSON.stringify(doc.data()));
+                this.vals.push(doc.data());
+            })
+        })
+        console.log(this.vals);
+        this.vals.sort(function(a,b){
+            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
+        })
+        this.thursday = this.vals;
+
+        this.vals.length=0;
+        getdata = firebase.firestore().collectionGroup("Friday");
+        check = getdata.where("Class_Id","==","1000");
+        check.get().then(result=>{
+            result.forEach(doc=>{
+                console.log(JSON.stringify(doc.data()));
+                this.vals.push(doc.data());
+            })
+        })
+        console.log(this.vals);
+        this.vals.sort(function(a,b){
+            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
+        })
+        this.friday = this.vals;
+
+
+        this.vals.length=0;
+        getdata = firebase.firestore().collectionGroup("Saturday");
+        check = getdata.where("Class_Id","==","1000");
+        check.get().then(result=>{
+            result.forEach(doc=>{
+                console.log(JSON.stringify(doc.data()));
+                this.vals.push(doc.data());
+            })
+        })
+        console.log(this.vals);
+        this.vals.sort(function(a,b){
+            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
+        })
+        this.saturday= this.vals;
+
         if(TodayDay=="Mon"){this.mon();}
         else if(TodayDay=="Tue"){this.tue();}
         else if(TodayDay=="Wed"){this.wed();}
@@ -82,30 +142,11 @@ export class displaytimetableComponent {
         this.satcol='white';
     }
     sun(): void{this.mon();}
-    mon(): void{this.rows=this.obj.Mon; this.color(); this.moncol='green';}
-    tue(): void{this.rows=this.obj.Tue; this.color(); this.tuecol='green';}
-    wed():void{this.rows=this.obj.Wed; this.color(); this.wedcol='green';}
-    thu(): void{this.rows=this.obj.Thu; this.color(); this.thucol='green';}
-    fri(): void{this.rows=this.obj.Fri; this.color(); this.fricol='green';}
-    sat(): void{this.rows=this.obj.Sat; this.color(); this.satcol='green';}
-    vals : Array<JSON> =[];
-    connect(): void{
-        this.vals.length=0;
-        const getdata = firebase.firestore().collectionGroup("Monday");
-        const check = getdata.where("Class_Id","==","1000");
-        check.get().then(result=>{
-            result.forEach(doc=>{
-                console.log(JSON.stringify(doc.data()));
-                this.vals.push(doc.data());
-            })
-        })
-        console.log(this.vals);
-        this.rows={};
-        this.vals.sort(function(a,b){
-            return parseInt(a["Sequence"])-parseInt(b["Sequence"]);
-        })
-        this.rows = this.vals;
-        console.log(this.rows);
-    }
+    mon(): void{this.rows=this.monday; this.color(); this.moncol='green';}
+    tue(): void{this.rows=this.tuesday; this.color(); this.tuecol='green';}
+    wed():void{this.rows=this.wednesday; this.color(); this.wedcol='green';}
+    thu(): void{this.rows=this.thursday; this.color(); this.thucol='green';}
+    fri(): void{this.rows=this.friday; this.color(); this.fricol='green';}
+    sat(): void{this.rows=this.saturday; this.color(); this.satcol='green';}
 
 }
